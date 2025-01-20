c09f22f109a1d939ddaa7a1158bb0d6c6f871fd3
zhoninho <zhanatbakirov05>
2025-01-21 01:19:06 +0600

    hw 5 finally

--- utilities/logic.py
+++ utilities/logic.py
@@ -1,20 +1,13 @@
 from random import randint
-<<<<<<< HEAD
-=======
 
->>>>>>> hw_5
 def guess_the_number(start_capital, attempts, number1, number2):
     capital = start_capital
     secret_number = randint(number1, number2)
     print("Добро пожаловать в игру 'Угадай число'")
     print(f"У вас {capital} монет. У вас {attempts} попыток, чтобы угадать число от {number1} до {number2}")
     attempt = 1
-<<<<<<< HEAD
-    while attempt <= attempts or capital <= 0:
-=======
 
     while attempt <= attempts and capital > 0:
->>>>>>> hw_5
         print(f"\nПопытка {attempt} из {attempts}")
         try:
             bet = int(input("Введите вашу ставку: "))
@@ -23,10 +16,7 @@ def guess_the_number(start_capital, attempts, number1, number2):
                 continue
 
             guess = int(input(f"Угадайте число: "))
-<<<<<<< HEAD
-=======
 
->>>>>>> hw_5
             if guess < number1 or guess > number2:
                 print(f"Число должно быть от {number1} до {number2}. Попробуйте снова")
                 continue
@@ -34,28 +24,6 @@ def guess_the_number(start_capital, attempts, number1, number2):
             if guess == secret_number:
                 winnings = bet * 2
                 capital += winnings
-<<<<<<< HEAD
-                print(f"Поздравляем! Вы выиграли {winnings} монет")
-                break
-            else:
-                capital -= bet
-                print(f"Неверно! Загаданное число { 'меньше' if guess > secret_number else 'больше' } {guess}")
-
-            if capital <= 0:
-                print(f"У вас закончились монеты")
-                break
-            elif attempt == attempts:
-                print(f"У вас закончились попытки")
-
-            print(f"Ваш текущий капитал: {capital} монет.")
-        except ValueError:
-            print("Пожалуйста, вводите только числа.")
-        attempt += 1
-    else:
-        print(f"Игра окончена! Загаданное число было: {secret_number}")
-
-    print(f"Ваш итоговый капитал: {capital} монет. Спасибо за игру!")
-=======
                 print(f"Поздравляем! Вы угадали число {secret_number}. Вы выиграли {winnings} монет")
                 break
             else:
@@ -76,4 +44,3 @@ def guess_the_number(start_capital, attempts, number1, number2):
 
     print(f"Игра окончена! Загаданное число было: {secret_number}\n"
           f"Ваш итоговый капитал: {capital} монет. Спасибо за игру!")
->>>>>>> hw_5
